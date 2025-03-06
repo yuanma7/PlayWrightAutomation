c605947624e8ed8c82b07f216594e97999df2f60
Yuan MA <mayuanmyra@gmail.com>
2025-03-06 20:57:28 +1100

    test 2

---
+++ tests/UIBasicstest.spec.js
@@ -0,0 +1,35 @@
+const {test} = require('@playwright/test');
+
+
+test('Browser Context Playwright Test', async ({browser}) =>
+    {
+        //playwright code
+    
+        // chrome - plugins/ cookies
+        const context = await browser.newContext();
+        // create a page in the browser
+        const page = await context.newPage();
+    
+        // url for the page
+        await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+        // step 1
+    
+        // step 2
+    
+        // step 3
+    
+    });
+
+test('Page Playwright Test', async ({page}) =>
+{
+    //playwright code
+
+    // url for the page
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+    // step 1
+
+    // step 2
+
+    // step 3
+
+});
