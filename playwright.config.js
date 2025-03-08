940f0fbac87521317607ed898eeb30446b630c01
Yuan MA <mayuanmyra@gmail.com>
2025-03-08 21:49:47 +1100

    section 4 video 12

--- playwright.config.js
+++ playwright.config.js
@@ -47,7 +47,7 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
+    /*
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,7 +1,7 @@
 const {test, expect} = require('@playwright/test');
 
 
-test('Browser Context Playwright Test', async ({browser}) =>
+test.only('Browser Context Playwright Test', async ({browser}) =>
     {
         //playwright code
     
@@ -10,13 +10,21 @@ test('Browser Context Playwright Test', async ({browser}) =>
         // create a page in the browser
         const page = await context.newPage();
     
-        // url for the page
+        // url for the page object
         await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
-        // step 1
+        // step 1, get the title of the page object
         console.log (await page.title());
-        // step 2
-    
-        // step 3
+        // step 2, css locate the username element on the page object, fill in a value
+        await page.locator("#username").fill("rahulshetty");
+        // step 3, css locate the password element on the page object, fill in a value
+        await page.locator("[type='password']").fill("learning");
+        // step 4, click sign in button
+        await page.locator("#signInBtn").click();
+        // step 5.1, wait until there is an element with style attribute paritally matching 'block', print the error message in console
+        console.log(await page.locator("[style*='block']").textContent());
+        // step 5.2, another way to verify the text
+        await expect(page.locator("[style*='block']")).toContainText('Incorrect');
+
     
     });
 
