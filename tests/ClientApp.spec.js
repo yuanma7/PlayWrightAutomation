9b896e3cfa8e118dcef51de23e10dc97904c591f
Yuan MA <mayuanmyra@gmail.com>
2025-03-15 22:45:53 +1100

    Video 29

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -78,21 +78,65 @@ test.skip('Login', async ({browser}) =>
     }
 );
 
-test('Client App Login', async ({page})=>
+test.only('Client App Login', async ({page})=>
 {
-    const products = page.locator(".card-body b");
+    const productName = 'ZARA COAT 3';
+    const products = page.locator(".card-body");
+    const email = "mayuanmyra@gmail.com"
     await page.goto("https://rahulshettyacademy.com/client");
     const Email = page.locator("#userEmail");
     const password = page.locator("#userPassword");
     const loginBtn = page.locator("#login");
         
-    await Email.fill("mayuanmyra@gmail.com");
+    await Email.fill(email);
     await password.fill("Test_123");
     await loginBtn.click();
     await page.waitForLoadState('networkidle');
 
     const titles = await page.locator(".card-body b").allTextContents();
-    console.log(titles);   
+    console.log(titles);
+
+    const count = await products.count();
+    for(let i = 0; i< count; ++i)
+    {
+        if(await products.nth(i).locator("b").textContent() === productName)
+        {
+            // add to card
+            await products.nth(i).locator("text=Add To Cart").click();
+            break;
+        }
+    }
+
+    await page.locator("[routerlink*='cart']").click();
+    await page.locator("div li").first().waitFor();
+    const bool = await page.locator("h3:has-text('ZARA COAT 3')").isVisible();
+    expect(bool).toBeTruthy();
+        
+    await page.locator("text=Checkout").click();
+    await page.locator("[placeholder*='Country']").pressSequentially('ind');
+
+    const dropdown = page.locator(".ta-results");
+    await dropdown.waitFor();
+
+    const optionsCount = await dropdown.locator("button").count();
+    for (let i = 0; i< optionsCount; ++i)
+    {
+        const text = await dropdown.locator("button").nth(i).textContent();
+        if(text.trim() === "India")
+        {
+            await dropdown.locator("button").nth(i).click();
+            break;
+        }
+    }
+    
+    await expect(page.locator(".user__name [type='text']").first()).toHaveText(email);
+    await page.locator(".action__submit").click();
+
+    await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
+
+    const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+    await console.log(orderId);
+
 
 }
 
