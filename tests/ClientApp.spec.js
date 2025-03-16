1a6c1e2c0fbde31b9f95f59e8915cc8e846586a4
Yuan MA <mayuanmyra@gmail.com>
2025-03-16 21:07:48 +1100

    Video 38

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -4,7 +4,7 @@
 const {test, expect} = require('@playwright/test');
 
 
-test.skip('Register', async ({browser}) =>
+test('Register', async ({browser}) =>
     {
         //playwright code
     
@@ -44,7 +44,7 @@ test.skip('Register', async ({browser}) =>
     }
 );
 
-test.skip('Login', async ({browser}) =>
+test('Login', async ({browser}) =>
     {
         //playwright code
     
@@ -78,7 +78,8 @@ test.skip('Login', async ({browser}) =>
     }
 );
 
-test.only('Client App Login', async ({page})=>
+// end to end testing, locators in css
+test('Client App Login', async ({page})=>
 {
     const productName = 'ZARA COAT 3';
     const products = page.locator(".card-body");
@@ -142,7 +143,6 @@ test.only('Client App Login', async ({page})=>
     await page.locator("tbody").waitFor();
     const rows = await page.locator("tbody tr");
  
- 
     for (let i = 0; i < await rows.count(); ++i) 
     {
        const rowOrderId = await rows.nth(i).locator("th").textContent();
@@ -155,7 +155,36 @@ test.only('Client App Login', async ({page})=>
     const orderIdDetails = await page.locator(".col-text").textContent();
     expect(orderId.includes(orderIdDetails)).toBeTruthy();
 
+});
 
-}
-
-);
+// end to end testing, locators with GetBy
+test.only('@Webst Client App login', async ({ page }) => {
+    //js file- Login js, DashboardPage
+    const email = "anshika@gmail.com";
+    const productName = 'ZARA COAT 3';
+    const products = page.locator(".card-body");
+    await page.goto("https://rahulshettyacademy.com/client");
+    await page.getByPlaceholder("email@example.com").fill(email);
+    await page.getByPlaceholder("enter your passsword").fill("Iamking@000");
+    await page.getByRole('button',{name:"Login"}).click();
+    await page.waitForLoadState('networkidle');
+    await page.locator(".card-body b").first().waitFor();
+    
+    await page.locator(".card-body").filter({hasText:"ZARA COAT 3"})
+    .getByRole("button",{name:"Add to Cart"}).click();
+  
+    await page.getByRole("listitem").getByRole('button',{name:"Cart"}).click();
+  
+    //await page.pause();
+    await page.locator("div li").first().waitFor();
+    await expect(page.getByText("ZARA COAT 3")).toBeVisible();
+  
+    await page.getByRole("button",{name :"Checkout"}).click();
+  
+    await page.getByPlaceholder("Select Country").pressSequentially("ind");
+  
+    await page.getByRole("button",{name :"India"}).nth(1).click();
+    await page.getByText("PLACE ORDER").click();
+  
+    await expect(page.getByText("Thankyou for the order.")).toBeVisible();
+ });
