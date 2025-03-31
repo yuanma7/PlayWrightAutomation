62e6ffb53cb971d91278cf1f5ad81ebe14d37377
Yuan MA <mayuanmyra@gmail.com>
2025-03-31 16:15:13 +1100

    Video 88

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -1,5 +1,7 @@
 const {test,expect} = require('@playwright/test')
 
+test.describe.configure({mode: 'parallel'});
+
 test("Popup Validations", async({page})=>
 {
     await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
